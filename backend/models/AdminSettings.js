f1d6ac57bff1ec4a6814b83f2382154bce8d373f
Raunak Chaurasiya <raunakchaurasiya35@gmail.com>
2026-02-16 15:54:17 +0530

    Revert "Merge branch 'main' of https://github.com/ankitsonu5/kfs2026"
    This reverts commit ad22ca71c429e8baf7427ce145b958c8c3272b17, reversing
    changes made to 0a2c01d9051132603ed9711ff52f844f2c48bcc3.

--- backend/models/AdminSettings.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const adminSettingsSchema = new mongoose.Schema({
-    siteName: String,
-    supportEmail: String,
-    deliveryCharge: Number,
-});
-
-module.exports = mongoose.model("AdminSettings", adminSettingsSchema);
